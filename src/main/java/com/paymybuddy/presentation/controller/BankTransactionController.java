3359dd7f66f819854a3cc31db44fca664338f4cb
Simon Linford <simon.linford@gmail.com>
2022-02-14 19:44:39 +0100

    correcting error in comment

--- src/main/java/com/paymybuddy/presentation/controller/BankTransactionController.java
+++ src/main/java/com/paymybuddy/presentation/controller/BankTransactionController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * RestController for /banktransaction endpoint
  *
- * Endpoint includes POST only
  */
 @RestController
 public class BankTransactionController {
